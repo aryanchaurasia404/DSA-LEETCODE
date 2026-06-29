725b598e32d5c97e23f05495d000efc55cd2d7e0
Aryan Chaurasia <143382123+aryanchaurasia404@users.noreply.github.com>
2026-06-29 13:09:54 +0530

    Time: 3 ms (97.95%), Space: 85.2 MB (11.21%) - LeetHub

---
+++ 1109-corporate-flight-bookings/1109-corporate-flight-bookings.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] corpFlightBookings(int[][] bookings, int n) {
+        int[] ans = new int[n];
+
+        for (int[] booking : bookings) {
+            int first = booking[0];
+            int last = booking[1];
+            int seats = booking[2];
+
+            ans[first - 1] += seats;
+
+            if (last < n) {
+                ans[last] -= seats;
+            }
+        }
+//prefix sum 
+        for (int i = 1; i < n; i++) {
+            ans[i] += ans[i - 1];
+        }
+
+        return ans;
+    }
+} 
