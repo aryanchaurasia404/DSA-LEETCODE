7caf466186e3f64ea369e56b50fb0c5d246fe30d
Aryan Chaurasia <143382123+aryanchaurasia404@users.noreply.github.com>
2026-07-06 09:58:09 +0530

    Time: 0 ms (100%), Space: 43.8 MB (45.44%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,39 +1,60 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
 
-        int i = 0;
-        int j = 0;
-        int id = 0;
+        // int i = 0;
+        // int j = 0;
+        // int id = 0;
 
-        int[] res = new int[m + n];
+        // int[] res = new int[m + n];
 
-        while (i < m && j < n) {
+        // while (i < m && j < n) {
 
-            if (nums1[i] <= nums2[j]) {
-                res[id] = nums1[i];
-                i++;
-                id++;
-            } else {
-                res[id] = nums2[j];
-                id++;
-                j++;
-            }
+        //     if (nums1[i] <= nums2[j]) {
+        //         res[id] = nums1[i];
+        //         i++;
+        //         id++;
+        //     } else {
+        //         res[id] = nums2[j];
+        //         id++;
+        //         j++;
+        //     }
+        // }
+
+        // while (j < n) {
+        //     res[id] = nums2[j];
+        //     id++;
+        //     j++;
+        // }
+
+        // while (i < m) {
+        //     res[id] = nums1[i];
+        //     id++;
+        //     i++;
+        // }
+
+        // for (int k = 0; k < m + n; k++) {
+        //     nums1[k] = res[k];
+        // }
+
+        int i=m-1;
+        int j=n-1;;
+        int x=m+n-1;
+
+        while(i >=0 && j >=0){
+          if(nums1[i] > nums2[j]){
+            nums1[x]=nums1[i];
+            i--;
+            x--;
+          }else{
+            nums1[x]=nums2[j];
+            j--;
+            x--;
+          }
         }
-
-        while (j < n) {
-            res[id] = nums2[j];
-            id++;
-            j++;
-        }
-
-        while (i < m) {
-            res[id] = nums1[i];
-            id++;
-            i++;
-        }
-
-        for (int k = 0; k < m + n; k++) {
-            nums1[k] = res[k];
+        while(j >=0){
+            nums1[x]=nums2[j];
+            j--;
+            x--;
         }
     }
 }
